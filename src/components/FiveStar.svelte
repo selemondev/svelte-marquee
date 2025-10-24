5a606bba032a42fea96bc204f54438f0828850f6
selemondev <selemondev19@gmail.com>
2025-10-24 04:59:17 -0600

    feat: migrate to Tailwind v4 and optimize CSS output

--- src/components/FiveStar.svelte
+++ src/components/FiveStar.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import StarIcon from './icons/StarIcon.svelte';
+    import StarIcon from "./icons/StarIcon.svelte";
 </script>
 
 <div class="flex items-center text-amber-400">
-	<StarIcon class="size-5" />
-	<StarIcon class="size-5" />
-	<StarIcon class="size-5" />
-	<StarIcon class="size-5" />
-	<StarIcon class="size-5" />
+    <StarIcon class="size-5" />
+    <StarIcon class="size-5" />
+    <StarIcon class="size-5" />
+    <StarIcon class="size-5" />
+    <StarIcon class="size-5" />
 </div>
--- src/lib/Marquee.svelte
+++ src/lib/Marquee.svelte
@@ -1,49 +1,48 @@
 <script lang="ts">
-	import { cn } from './utils.js';
-	type Direction = 'left' | 'up';
-	export let direction: Direction = 'left';
-	export let pauseOnHover: boolean = false;
-	export let reverse: boolean = false;
-	export let fade: boolean = false;
-	export let innerClassName: string = '';
-	export let numberOfCopies: number = 2;
+    import { cn } from "./utils.js";
+    type Direction = "left" | "up";
+    export let direction: Direction = "left";
+    export let pauseOnHover: boolean = false;
+    export let reverse: boolean = false;
+    export let fade: boolean = false;
+    export let innerClassName: string = "";
+    export let numberOfCopies: number = 2;
 </script>
 
 <div
-	class={cn(`group flex gap-[1rem] overflow-hidden ${$$restProps.class}`, {
-		'flex-row': direction === 'left',
-		'flex-col': direction !== 'left',
-	}
-	)}
-	style={`mask-image: ${
-		fade
-			? `linear-gradient(${
-					direction === 'left' ? 'to right' : 'to bottom'
-				}, transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)`
-			: 'none'
-	};
+    class={cn(`group flex gap-[1rem] overflow-hidden ${$$restProps.class}`, {
+        "flex-row": direction === "left",
+        "flex-col": direction !== "left",
+    })}
+    style={`mask-image: ${
+        fade
+            ? `linear-gradient(${
+                  direction === "left" ? "to right" : "to bottom"
+              }, transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)`
+            : "none"
+    };
 	  -webkit-mask-image: ${
-			fade
-				? `linear-gradient(${
-						direction === 'left' ? 'to right' : 'to bottom'
-					}, transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)`
-				: 'none'
-		};
+          fade
+              ? `linear-gradient(${
+                    direction === "left" ? "to right" : "to bottom"
+                }, transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)`
+              : "none"
+      };
 	  `}
 >
-	{#each Array(numberOfCopies).fill(0) as _, i (i)}
-		<div
-			class={cn(
-				'flex justify-around gap-[1rem] [--gap:1rem] shrink-0',
-				direction === 'left'
-					? 'animate-marquee-left flex-row'
-					: 'animate-marquee-up flex-col',
-				pauseOnHover && 'group-hover:[animation-play-state:paused]',
-				reverse && 'direction-reverse',
-				innerClassName
-			)}
-		>
-			<slot />
-		</div>
-	{/each}
+    {#each Array(numberOfCopies).fill(0) as _, i (i)}
+        <div
+            class={cn(
+                "flex justify-around gap-[1rem] [--gap:1rem] shrink-0",
+                direction === "left"
+                    ? "animate-marquee-left flex-row"
+                    : "animate-marquee-up flex-col",
+                pauseOnHover && "group-hover:[animation-play-state:paused]",
+                reverse && "direction-reverse",
+                innerClassName,
+            )}
+        >
+            <slot />
+        </div>
+    {/each}
 </div>
